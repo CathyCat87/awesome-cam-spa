bb95b31f1966af7ea52af87ad858a87a505318b3
--global <--global>
2023-04-05 13:44:04 -0600

    Added Auth0 login button.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LoginButton from "../components/buttons/login-button";
 export default function HomePage() {
     return (
         <>
             <h1>Hey from HomePage</h1>
             <p>This is your awesome HomePage subtitle</p>
+            <LoginButton />
         </>
     );
 }
