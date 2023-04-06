29068370d08ba0e7bc716ffb93da6afd624d06f4
--global <--global>
2023-04-06 13:26:40 -0600

    Added page-footer-hyperlink component to construct page footer.

---
+++ src/components/page-footer-hyperlink.js
@@ -0,0 +1,13 @@
+import React from "react";
+export const PageFooterHyperlink = ({ children, path }) =>{
+  return (
+      <a
+          className="page-footer__hyperlink"
+          href={ path }
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          { children }
+      </a>
+  );
+};
