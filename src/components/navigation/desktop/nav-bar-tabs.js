73acc0d581b3d101292177a7379f875c6f333e9b
--global <--global>
2023-04-06 17:40:12 -0600

    Updated to move comment from printable region (bug).

--- src/components/navigation/desktop/nav-bar-tabs.js
+++ src/components/navigation/desktop/nav-bar-tabs.js
@@ -3,10 +3,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export const NavBarTabs = () =>{
     const { isAuthenticated } = useAuth0();
-
+    //TODO
     return (
       <div className="nav-bar__tabs">
-          //TODO
+
       </div>
     );
 };
