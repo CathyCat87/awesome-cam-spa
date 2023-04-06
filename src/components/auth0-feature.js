1068a1674934f913810b07d90a8b123e2a526249
--global <--global>
2023-04-06 10:24:36 -0600

    Added auth0-feature html component for auth0-features blcok.

---
+++ src/components/auth0-feature.js
@@ -0,0 +1,15 @@
+import React from "react";
+export const Auth0Feature = ({ title, description, resourceURL, icon }) =>(
+  <a href={ resourceURL }
+     className="auth0-feature"
+     target="_blank"
+     rel={"noopener noferrer"}>
+      <h3 className="auth0-feature__headline">
+          <img className="auth0-feature__icon"
+               src={ icon }
+               alt="external link icon"/>
+          { title }
+      </h3>
+      <p className="auth0-feature__description">{ description }</p>
+  </a>
+);
