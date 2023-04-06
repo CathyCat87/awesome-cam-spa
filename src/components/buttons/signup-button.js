61615975745c58e8156e9c2a4630e998819ad8da
--global <--global>
2023-04-06 17:49:09 -0600

    Added button for sign-up, updated route on login-button.

--- src/components/buttons/signup-button.js
+++ src/components/buttons/signup-button.js
@@ -2,27 +2,24 @@ import React from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
-export const LoginButton = () => {
+export const SignupButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    const handleLogin = async () => {
+    const handleSignUp = async () => {
         await loginWithRedirect({
             appState: {
                 returnTo: "/",
             },
             authorizationParams: {
                 prompt: "login",
+                screen_hint: "signup"
             },
         });
     };
 
     return (
-        <button className="button__login" onClick={ handleLogin }>
-            Log In
-        </button>
-        );
-
-
+      <button className="button__sign-up" onClick={ handleSignUp }>
+          Sign Up
+      </button>
+    );
 };
-
-export default LoginButton;
