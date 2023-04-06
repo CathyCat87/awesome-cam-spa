92312db35d5fd82d22a8513c1e5579675e5967db
--global <--global>
2023-04-06 17:51:38 -0600

    Added whitespace.

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -6,21 +6,12 @@ export const HeroBanner = () => {
     return (
         <div className="hero-banner hero-banner--pink-yellow">
             <div className="hero-banner__logo">
-                <img className="hero-banner__image" src={logo} alt="Awesome-Cam logo" />
+                <img className="hero-banner__image" src={ logo } alt="Awesome-Cam logo" />
             </div>
             <h1 className="hero-banner__headline">Welcome to Awesome-Cam!</h1>
             <p className="hero-banner__description">
                 Awesome-cam is a security camera hosted on a RaspberryPI completed for <strong>CS490CO Winter 2023</strong>.
             </p>
-            <a
-                id="code-sample-link"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://developer.auth0.com/resources/code-samples/spa/react/basic-authentication"
-                className="button button--secondary"
-            >
-                Check out the React code sample
-            </a>
         </div>
     );
 };
