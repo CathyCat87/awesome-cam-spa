f413cac40fc29a386f6fd9de34b902b5ad859036
--global <--global>
2023-04-06 17:29:20 -0600

    Fixed bug.

---
+++ src/pages/LogoutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Hey from Logout Page</h1>
+            <p>This is your awesome Logout Page subtitle</p>
+        </>
+    );
+}
