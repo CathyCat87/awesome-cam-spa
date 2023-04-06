907795201cb1c9c5eb90312e8db79cfe278f43df
--global <--global>
2023-04-05 22:51:53 -0600

    Updated for v6 change.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { useAuth0  } from "@auth0/auth0-react";
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+import { PageLoader } from "./components/page-loader";
+import { AuthenticationGuard } from "./components/authentication-guard";
+
+import { HomePage } from "./pages/HomePage";
+
+export const App = () =>{
+  const { isLoading } = useAuth0();
+
+  if ( isLoading )
+      return (
+        <div className="page-layout">
+            <PageLoader />
+        </div>
+      );
+
+  return (
+    <Routes>
+        <Route
+            path="/"
+            element={ <HomePage /> }
+        />
+    </Routes>
+  );
+};
+
+
+/*import React from "react";
+import { Route, Routes } from "react-router-dom";
 // We will create these two pages in a moment
-import HomePage from "./pages/HomePage"
-import UserPage from "./pages/UserPage"
+
+import DashboardPage from "./pages/DashboardPage";
+import { HomePage } from "./pages/HomePage";
+import LogoutPage from "./pages/LogoutPage";
+import UserPage from "./pages/UserPage";
+
+import { AuthenticationGuard } from "./components/authentication-guard";
+
 export default function App() {
   return (
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/:id" component={UserPage} />
-      </Switch>
-  )
-}
+      <Routes>
+          <Route exact
+                 path="/"
+                 element={ HomePage } />
+          <Route path="/dashboard"
+                 element={<AuthenticationGuard component={ DashboardPage } />}
+          />
+          <Route path="/profile"
+                 element={<AuthenticationGuard component={ UserPage } />}
+          />
+          <Route path="/logout" element={ LogoutPage } />
+      </Routes>
+  );
+}*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react"
-import { render } from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+import "./styles/styles.css";
 
-render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.querySelector("#root")
-)
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Auth0ProviderWithNavigate>
+                <App />
+            </Auth0ProviderWithNavigate>
+        </BrowserRouter>
+    </React.StrictMode>
+);
