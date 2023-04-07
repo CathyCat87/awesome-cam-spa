f6461348489a57b37394c7afc9d6746d67380d81
CathyCat87 <cathy101296@hotmail.com>
2023-04-07 12:37:53 -0600

    Added mobile nav bar to page layout.

--- src/components/page-layout.js
+++ src/components/page-layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavBar } from "./navigation/desktop/nav-bar";
 import { PageFooter } from "./page-footer";
-
+import { MobileNavBar } from "./navigation/mobile/mobile-nav-bar";
 
 export const PageLayout = ({ children }) => {
 
     return (
         <div className="page-layout">
-            <NavBar />:q
-
+            <NavBar />
+            <MobileNavBar />
             { children }
             <PageFooter />
         </div>
