206bdc2bdca879798e4b318ed9f4823e5c531413
--global <--global>
2023-04-06 21:20:05 -0600

    Updated.

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -5,12 +5,12 @@ export const NavBarBrand = () => {
     return (
         <div className="nav-bar__brand">
             <NavLink to="/">
-                <img className="nav-bar__logo"
-                     //TODO
-                     src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
-                     alt="Awesome-Cam Logo"
-                     width="122"
-                     height="36"
+                <img
+                    className="nav-bar__logo"
+                    src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
+                    alt="Auth0 shield logo"
+                    width="122"
+                    height="36"
                 />
             </NavLink>
         </div>
