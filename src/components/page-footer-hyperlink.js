69cedf50cd21258eff7194541873083da474af44
CathyCat87 <cathy101296@hotmail.com>
2023-04-08 01:50:46 -0600

    Prettied up the footer with some fontawesome icons and changed up the links.

--- src/components/page-footer-hyperlink.js
+++ src/components/page-footer-hyperlink.js
@@ -1,5 +1,6 @@
 import React from "react";
-export const PageFooterHyperlink = ({ children, path }) =>{
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+export const PageFooterHyperlink = ({ children, path, icon }) =>{
   return (
       <a
           className="page-footer__hyperlink"
@@ -7,6 +8,12 @@ export const PageFooterHyperlink = ({ children, path }) =>{
           target="_blank"
           rel="noopener noreferrer">
           { children }
+          { icon && (
+              <>
+                  &nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon icon={icon} />
+              </>
+          )}
       </a>
   );
 };
--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { PageFooterHyperlink } from "./page-footer-hyperlink";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faBookOpen, faCopyright } from "@fortawesome/free-solid-svg-icons";
+
+
 export const PageFooter = () => {
   const resourceList = [
       {
-          path: "https://www.google.com/search?q=todo",
-          label: "ToDo"
+          path: "https://github.com/CathyCat87/awesome-cam-spa",
+          label: "SPA Source",
+          icon: faGithub,
       },
       {
-          path: "https://www.google.com/search?q=todo",
-          label: "ToDo"
+          path: "https://github.com/CathyCat87/awesome-cam-server",
+          label: "Python Server(s) Source",
+          icon: faGithub,
       },
       {
-          path: "https://www.google.com/search?q=todo",
-          label: "ToDo"
+          path: "https://github.com/CathyCat87/awesome-cam-pi",
+          label: "PI Source",
+          icon: faGithub,
       },
       {
-          path: "https://www.google.com/search?q=todo",
-          label: "ToDo"
+          path: "/references",
+          label: "References",
+          icon: faBookOpen,
       },
   ];
 
@@ -25,27 +35,25 @@ export const PageFooter = () => {
       <footer className="page-footer">
           <div className="page-footer-grid">
               <div className="page-footer-grid__info">
-                  <div className="page-footer-info__message">
-                      <p className="page-footer-message__headline">
-                          <span>Awesome-cam has been brought to you by&nbsp;</span>
-                          <PageFooterHyperlink path="https://www.linkedin.com/in/cathy-zeng-earnshaw-59701283/">
-                              Cathy Zeng-Earnshaw
-                          </PageFooterHyperlink>.
-                      </p>
-                  </div>
 
-                  <div className="page-footer-info__resource-list">
-                      {resourceList.map((resource) => (
-                          <div
-                              key={resource.path}
-                              className="page-footer-info__resource-list-item"
-                          >
-                              <PageFooterHyperlink path={resource.path}>
-                                  {resource.label}
-                              </PageFooterHyperlink>
-                          </div>
-                      ))}
-                  </div>
+                      <p className="page-footer-message__headline">
+
+                      </p>
+
+
+
+              </div>
+              <div className="page-footer-info__resource-list">
+                  { resourceList.map((resource) => (
+                      <div
+                          key={ resource.path }
+                          className="page-footer-info__resource-list-item"
+                      >
+                          <PageFooterHyperlink path={ resource.path } icon={ resource.icon }>
+                              { resource.label }
+                          </PageFooterHyperlink>
+                      </div>
+                  ))}
               </div>
               <div className="page-footer-grid__brand">
                   <div className="page-footer-brand">
@@ -59,6 +67,13 @@ export const PageFooter = () => {
                       <PageFooterHyperlink path="https://www.awesome-cam.ca">
                           Awesome-Cam
                       </PageFooterHyperlink>
+                      <span>
+                          &nbsp;has been brought to you by&nbsp;
+                          <PageFooterHyperlink path="https://www.linkedin.com/in/cathy-zeng-earnshaw-59701283/">
+                              Cathy Zeng-Earnshaw <FontAwesomeIcon icon={ faLinkedin } />
+                          </PageFooterHyperlink>
+                          &nbsp;&nbsp;<FontAwesomeIcon icon={ faCopyright } />2023
+                      </span>
                   </div>
               </div>
           </div>
