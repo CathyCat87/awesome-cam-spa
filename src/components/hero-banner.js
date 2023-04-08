44127e8f8b7a6e7dc86cb2918aef6882ff1c18d9
CathyCat87 <cathy101296@hotmail.com>
2023-04-08 11:20:52 -0600

    Fixed double word.

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -10,7 +10,7 @@ export const HeroBanner = () => {
             </div>
             <h1 className="hero-banner__headline">Welcome to Awesome-Cam!</h1>
             <p className="hero-banner__description">
-                Awesome-cam is a security camera hosted on a RaspberryPI with on-board image recognition, centralized server, and React Single Page Application application completed for <strong>CS490CO(Winter 2023)</strong> at the University of Regina.
+                Awesome-cam is a security camera hosted on a RaspberryPI with on-board image recognition, centralized server, and React Single Page Application completed for <strong>CS490CO(Winter 2023)</strong> at the University of Regina.
             </p>
         </div>
     );
