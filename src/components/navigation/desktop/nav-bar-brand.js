a659b3b50be8c57f44b6d6897d71ee76e2fc3c99
CathyCat87 <cathy101296@hotmail.com>
2023-04-08 00:54:25 -0600

    White space.

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -6,14 +6,15 @@ export const NavBarBrand = () => {
         <div className="nav-bar__brand">
             <NavLink to="/">
                 <span className="messages-grid__header">
-                <img
-                    className="nav-bar__logo"
-                    src="./linked-camera.svg"
-                    alt="Awesome-cam Camera Logo"
-                    width="36"
-                    height="36"
-                />
-                Awesome-Cam</span>
+                    <img
+                        className="nav-bar__logo"
+                        src="./linked-camera.svg"
+                        alt="Awesome-cam Camera Logo"
+                        width="36"
+                        height="36"
+                    />
+                    Awesome-Cam
+                </span>
             </NavLink>
         </div>
     );
