48999f258b24cf08e3b905711338330f9719081f
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 16:55:00 -0600

    Changed page format to meet new application architecture.

--- src/pages/dashboard-page.js
+++ src/pages/dashboard-page.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { PageLayout } from "../components/page-layout";
 
-export default function HomePage() {
+export const DashboardPage = () => {
     return (
-        <>
+        <PageLayout>
             <h1>Hey from Dashboard</h1>
-            <p>This is your awesome Dashboar subtitle</p>
-        </>
+            <p>This is your awesome Dashboard subtitle</p>
+            <p>Maybe current/most recent picture then a table with a list of events to click on for details.</p>
+        </PageLayout>
     );
 }
