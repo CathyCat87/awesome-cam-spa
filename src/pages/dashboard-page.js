7181644429bb1ab50ec47b60ca80601f23051713
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 16:44:41 -0600

    Refactored dashboard page to new naming format.

---
+++ src/pages/dashboard-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Hey from Dashboard</h1>
+            <p>This is your awesome Dashboar subtitle</p>
+        </>
+    );
+}
