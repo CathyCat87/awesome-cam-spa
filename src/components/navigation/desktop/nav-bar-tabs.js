3556dc009d1ee829e395faf94e3039673e056505
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 18:28:10 -0600

    Added profile page tab (Authenticated).

--- src/components/navigation/desktop/nav-bar-tabs.js
+++ src/components/navigation/desktop/nav-bar-tabs.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import {NavBarTab} from "./nav-bar-tab";
 
 export const NavBarTabs = () =>{
     const { isAuthenticated } = useAuth0();
-    //TODO
     return (
       <div className="nav-bar__tabs">
-
+          { isAuthenticated && (
+            <>
+                <NavBarTab path="/dashboard" label="Dashboard" />
+                <NavBarTab path="/profile" label="Profile" />
+            </>
+          )}
       </div>
     );
 };
