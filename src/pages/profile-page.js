b091fe51d005e3407f64e2f65a7c94e14555eda8
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 18:22:41 -0600

    Styled page to match.

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { PageLayout } from "../components/page-layout";
 //import { useParams } from "react-router-dom";
 export default function ProfilePage() {
 //    let { id } = useParams();
     return (
-        <>
+        <PageLayout>
             <h1>Hello there user.</h1>
             <p>This is your awesome User Profile page</p>
-        </>
+        </PageLayout>
     );
 }
