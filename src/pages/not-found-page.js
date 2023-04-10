e068789e3db02f573f49fe0d531b848c64a1a270
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 18:43:21 -0600

    Added not found page for 404s.

---
+++ src/pages/not-found-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageLayout } from "../components/page-layout";
+
+export const NotFoundPage = () => {
+    return (
+        <PageLayout>
+            <h1>Page Not Found.</h1>
+            <p>The page you are looking for is not found.</p>
+        </PageLayout>
+    );
+}
