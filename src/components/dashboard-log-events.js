488b3b69549b886687d54962c04b7ad886730ad8
CathyCat87 <cathy101296@hotmail.com>
2023-04-10 16:33:34 -0600

    Removed redundancy.

--- src/components/dashboard-log-events.js
+++ src/components/dashboard-log-events.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const DashboardElementLogEvents = () =>{
+export const DashboardLogEvents = () =>{
     return (
         <div>
 
--- src/components/dashboard-profile.js
+++ src/components/dashboard-profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const DashboardElementProfile = () => {
+export const DashboardProfile = () => {
 
     return (
         <div>
