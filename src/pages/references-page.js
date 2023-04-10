bd624dbc1143a947bf1a4f5738c76d6061c561cf
CathyCat87 <cathy101296@hotmail.com>
2023-04-09 18:32:04 -0600

    Added placeholder page for refrence link in footer.

---
+++ src/pages/references-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageLayout } from "../components/page-layout";
+
+export const ReferencesPage = () => {
+    return (
+        <PageLayout>
+            <h1>TODO FILL IN PROJECT REFERENCES AND LINKS</h1>
+            <p>TODO THIS IS A PLACE HOLDERS</p>
+        </PageLayout>
+    );
+};
