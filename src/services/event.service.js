c1c4d1d3d9618e455c834b4e1b7f42b7526b9592
CathyCat87 <cathy101296@hotmail.com>
2023-04-11 12:25:49 -0600

    Styling

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -2,9 +2,9 @@ import { callExternalAPI } from "./external-api.service";
 
 const apiServerURL = process.env.REACT_APP_API_SERVER_URL;
 
-export const getEvents = async () => {
+export const getAPIEvents = async () => {
     const config = {
-        url: `${apiServerURL}/events`,
+        url: `${ apiServerURL }/events`,
         method: "GET",
         headers: {
             "content-type": "application/json",
