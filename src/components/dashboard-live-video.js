2a8940799dfc2cac75272f1d92dec09154ebb5c6
CathyCat87 <cathy101296@hotmail.com>
2023-04-11 12:08:41 -0600

    Added live stream from camera on RaspBerry PI.

--- src/components/dashboard-live-video.js
+++ src/components/dashboard-live-video.js
@@ -4,6 +4,7 @@ export const DashboardLiveVideo = () => {
     return (
         <div className="col-md-3">
             LIVE VIDEO
+            <img src="http://192.168.2.54:8000/stream.mjpg" width="100%" />
         </div>
     );
 };
