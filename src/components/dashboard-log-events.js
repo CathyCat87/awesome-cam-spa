f3cf9d7d0304722cc0e79e38d96dd66e9a4941c4
CathyCat87 <cathy101296@hotmail.com>
2023-04-11 12:28:21 -0600

    Updated for pulling events list.

--- src/components/dashboard-log-events.js
+++ src/components/dashboard-log-events.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+
+import { getAPIEvents } from "../services/event.service";
+import { CodeSnippet } from "./code-snippet";
 
 export const DashboardLogEvents = () =>{
-    return (
 
+    const [ events, setEvents ] = useState("");
+
+    useEffect(() => {
+        let isMounted = true;
+
+        const getEvents = async () => {
+            const { data, error } = await getAPIEvents();
+
+            if ( !isMounted )
+                return;
+
+            if ( data )
+                setEvents(JSON.stringify(data, null, 2))
+
+            if  ( error )
+                setEvents(JSON.stringify(error, null, 2))
+        };
+
+        getEvents();
+
+        return () => {
+            isMounted = false;
+        };
+
+    }, []);
+    return (
         <div className="col-md-12">
-        Event Log
+            <div className="container">
+                <div className="row">
+                    <div className="col-12" align="center">
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12" align="center">
+                        <b>Event Log</b>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <CodeSnippet title="Events" code={ events } />
         </div>
+
+
 
     );
 };
