de1b896679c04927718464e1cc2305896fbc5d85
CathyCat87 <cathy101296@hotmail.com>
2023-04-10 23:20:45 -0600

    Small Update to mock-up.

--- src/components/dashboard-camera-status.js
+++ src/components/dashboard-camera-status.js
@@ -8,16 +8,21 @@ export const DashboardCameraStatus = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-12" align="center">
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12" align="center">
                         <b>Camera Status</b>
                         <hr />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-6">
-                        <FontAwesomeIcon icon={ faCamera } size="2xl"/> Camera 1 (Prototype)<br />
+                        <FontAwesomeIcon icon={ faCamera } size="2xl" /> Camera 1 (Prototype)<br />
                     </div>
                     <div className="col-6" align="right">
-                        <FontAwesomeIcon icon={ faLocationDot } size="2xl"/> South West Gate<br />
+                        <FontAwesomeIcon icon={ faLocationDot } size="2xl" /> South West Gate<br />
                     </div>
                 </div>
                 <div className="row">
@@ -27,13 +32,13 @@ export const DashboardCameraStatus = () => {
                 </div>
                 <div className="row">
                     <div className="col-4" align="left">
-                        <FontAwesomeIcon icon={ faBatteryFull } size="2xl"/>
+                        <FontAwesomeIcon icon={ faBatteryFull } size="2xl" />
                     </div>
                     <div className="col-4" align="center">
-                        <FontAwesomeIcon icon={ faPlugCircleBolt } size="2xl"/>
+                        <FontAwesomeIcon icon={ faPlugCircleBolt } size="2xl" />
                     </div>
                     <div className="col-4" align="right">
-                        <FontAwesomeIcon icon={ faSignal5 } size="2xl"/>
+                        <FontAwesomeIcon icon={ faSignal5 } size="2xl" />
                     </div>
                 </div>
                 <div className="row">
