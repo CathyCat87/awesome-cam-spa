80f8769c3a8618b6cc0d77afc266f78f78610371
CathyCat87 <cathy101296@hotmail.com>
2023-04-11 09:34:30 -0600

    Added service to get the events list from a public (non-secure) URL.

---
+++ src/services/event.service.js
@@ -0,0 +1,20 @@
+import { callExternalAPI } from "./external-api.service";
+
+const apiServerURL = process.env.REACT_APP_API_SERVER_URL;
+
+export const getEvents = async () => {
+    const config = {
+        url: `${apiServerURL}/events`,
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+        },
+    };
+
+    const { data, error } = await callExternalAPI( { config } );
+
+    return {
+        data: data || null,
+        error,
+    };
+};
